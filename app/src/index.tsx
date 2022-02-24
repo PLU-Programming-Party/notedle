ad24347e36a3244c4259b7879ee5b07cf05c122d
Kevin0x00 <thompskr@plu.edu>
2022-02-23 21:51:49 -0800

    Start

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -12,6 +12,7 @@ ReactDOM.render(
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://snowpack.dev/concepts/hot-module-replacement
-if (import.meta.hot) {
+if (import.meta.hot) 
+{
   import.meta.hot.accept();
 }
