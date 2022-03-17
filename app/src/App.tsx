21beb28772ee5a51377f6f2d37ae1ab03716c9df
AJ Moore <mooreajj@gmail.com>
2022-03-16 22:07:28 -0700

    feat: notes 'n sounds! added buttons for each note added sound when buttons are clicked added functionality to remove current notes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,37 +1,53 @@
 import React from 'react';
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
+import * as Tone from 'tone'
 
-interface AppProps {}
+interface AppProps { }
 
-function App({}: AppProps)
-{
+function App({ }: AppProps) {
 
-  const hotCrossBuns = ["Bb", "A", "G", "Bb", "A", "G"]
-  const happyBirthday = ["A", "A", "B", "A", "D", "C#"]
+  const allNotes = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4"] // for future: C5, C#5, etc.?
+  const targetSong = ["B", "A", "G", "B", "A", "G"] // hot cross buns
+  const synth = new Tone.Synth().toDestination();
+  
   const [words, setWords] = useState("")
-  const [actual, setActual] = useState(hotCrossBuns.map(() => ""))
+  const [actual, setActual] = useState<string[]>([])
 
-  function handleChange (i: number, value: string){
+  function playNote(value: string) {
+    synth.triggerAttackRelease(value, "4n", Tone.now());
+  }
+
+  function handleChange(value: string) {
+    playNote(value)
     const guessedNotes = actual.slice()
-    guessedNotes[i] = value
+    guessedNotes.push(value)
     setActual(guessedNotes)
+  }
+
+  function removeNote() {
+    const notes = actual.slice()
+    notes.pop()
+    setActual(notes)
+  }
+
+  function removeAllNotes() {
+    setActual([])
   }
 
   return (
     <div className="App">
-      <div> {hotCrossBuns.map((aBun, i) => {
-        return <input  
-        onChange= {(event)=> handleChange(i, event.target.value)}
-        />})
-      }
-      </div>
-      <p> {actual.join(", ")} </p>
-      <input 
+      <p><button>Play</button> {actual.join(", ")} </p>
+      <input
         type="text"
-        onChange={({target: {value}})=>setWords(value)}/>
+        onChange={({ target: { value } }) => setWords(value)} />
       <p>{words}</p>
-      <p>another <span style={{fontWeight:"bold"}}>Awesome</span></p>
+      <p>another <span style={{ fontWeight: "bold" }}>Awesome</span></p>
+      <div>
+        {allNotes.map((note) => <button onClick={() => handleChange(note)} disabled={actual.length >= targetSong.length}>{note}</button>)}
+        <button onClick={() => removeNote()} disabled={actual.length == 0} id="deleteButton">Delete</button>
+        <button onClick={() => removeAllNotes()} disabled={actual.length == 0} id = "deleteAllButton">Delete All</button>
+      </div>
     </div>
   );
 }
