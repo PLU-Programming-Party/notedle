249c163d570a22e7158a5e89e18c1daed86d7421
Duffy <duffy.anderson@yahoo.com>
2022-03-23 22:05:57 -0700

    Thank you noel, you've been missed. arrrrrrr. the play button works, and delay almost does. :)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,7 +8,7 @@ interface AppProps { }
 function App({ }: AppProps) {
 
   const allNotes = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4"] // for future: C5, C#5, etc.?
-  const targetSong = ["B", "A", "G", "B", "A", "G"] // hot cross buns
+  const targetSong: Array<[string,number]> = [["B",1], ["A",1],["G",4],["B",2], ["A",3],["G",1],["B",1],["A",1],["G",1],["B",1]] // hot cross buns
   const synth = new Tone.Synth().toDestination();
   
   const [words, setWords] = useState("")
@@ -16,6 +16,17 @@ function App({ }: AppProps) {
 
   function playNote(value: string) {
     synth.triggerAttackRelease(value, "4n", Tone.now());
+  }
+
+  function playAllNotes(notesArr: string[]){
+    const now = Tone.now()
+    var num = 0
+    var sum =0
+    for(const i of notesArr){
+      synth.triggerAttackRelease(i, targetSong[num][1],sum)
+      num++
+      sum = sum + targetSong[num][1]
+    }
   }
 
   function handleChange(value: string) {
@@ -37,12 +48,12 @@ function App({ }: AppProps) {
 
   return (
     <div className="App">
-      <p><button>Play</button> {actual.join(", ")} </p>
+      <p><button onClick={()=> playAllNotes(actual)}>Play</button> {actual.join(", ")} </p>
       <input
         type="text"
         onChange={({ target: { value } }) => setWords(value)} />
       <p>{words}</p>
-      <p>another <span style={{ fontWeight: "bold" }}>Awesome</span></p>
+      <p>It's <span style={{ fontWeight: "bold" }}>Awesome</span></p>
       <div>
         {allNotes.map((note) => <button onClick={() => handleChange(note)} disabled={actual.length >= targetSong.length}>{note}</button>)}
         <button onClick={() => removeNote()} disabled={actual.length == 0} id="deleteButton">Delete</button>
