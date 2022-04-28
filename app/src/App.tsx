bde127aac994940c2d42d843c1b647dc871ebec5
AJ Moore <mooreajj@gmail.com>
2022-04-27 22:08:01 -0700

    remove input box, better styling

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { useState } from 'react';
 import * as Tone from 'tone'
 import { Box } from './components/Box'
-import { makePrettyColors } from './SmartStuff'
+import { makePrettyColors, PrettyColor } from './SmartStuff'
 
 interface AppProps { }
 
@@ -15,28 +15,28 @@ type Note = NoteTuple[number];  // "hearts" | "diamonds" | "spades" | "clubs"
 
 function App({ }: AppProps) {
 
-  const targetSong: Array<[Note, number]> = [["B4",.3], ["A4",.3],["G4",.6],["B4",.3], ["A4",.3],["G4",.6],["B4",.3],["A4",.3],["G4",.6]] // hot cross buns
+  const targetSong: Array<[Note, number]> = [["B4", .3], ["A4", .3], ["G4", .6], ["B4", .3], ["A4", .3], ["G4", .6], ["B4", .3], ["A4", .3], ["G4", .6]] // hot cross buns
   const synth = new Tone.Synth().toDestination();
 
   //this is target song but just the notes
   const simplify = targetSong.map(current => current[0])
-  
-  const [words, setWords] = useState("")
+
+  // const [words, setWords] = useState("")
   const [currentGuess, setCurrentGuess] = useState<string[]>([])
   const [previousGuessesUwU, setPreviousGuessesOwO] = useState<string[][]>([])
-  const [previousGuessColorsOwO, setPreviousGuessColorsUwU] = useState<string[][]>([])
+  const [previousGuessColorsOwO, setPreviousGuessColorsUwU] = useState<PrettyColor[][]>([])
 
   function playNote(value: string) {
     synth.triggerAttackRelease(value, "4n", Tone.now());
   }
 
-  function playAllNotes(notesArr: string[]){
+  function playAllNotes(notesArr: string[]) {
     const now = Tone.now()
     var sum = 0
-    for(let i = 0; i < notesArr.length; i++){
+    for (let i = 0; i < notesArr.length; i++) {
       synth.triggerAttackRelease(notesArr[i], targetSong[i][1], now + sum)
       sum = sum + targetSong[i][1]
-    } 
+    }
   }
 
   function handleChange(value: string) {
@@ -57,7 +57,7 @@ function App({ }: AppProps) {
   }
 
   function handleSubmit() {
-    setPreviousGuessesOwO(prev => {return prev.concat([currentGuess])})
+    setPreviousGuessesOwO(prev => { return prev.concat([currentGuess]) })
     setPreviousGuessColorsUwU(prev => prev.concat([makePrettyColors(simplify, currentGuess)]))
     removeAllNotes()
   }
@@ -65,21 +65,21 @@ function App({ }: AppProps) {
   return (
     <div className="App">
       <div>
-        {previousGuessesUwU.map((guess,i) => {
-          return <Box guess={guess} color={previousGuessColorsOwO[i]}/>
+        {previousGuessesUwU.map((guess, i) => {
+          return <Box guess={guess} color={previousGuessColorsOwO[i]} />
         })}
       </div>
-      <p><button onClick={()=> playAllNotes(currentGuess)}>Play</button> {currentGuess.join(", ")} </p>
-      <input
+      <p><button onClick={() => playAllNotes(currentGuess)}>Play</button> {currentGuess.join(", ")} </p>
+      {/* <input
         type="text"
         onChange={({ target: { value } }) => setWords(value)} />
-      <p>{words}</p>
-      <p>It's <span style={{ fontWeight: "bold" }}>Awesome</span></p>
+      <p>{words}</p> */}
+      <p><span style={{ fontWeight: "bold", color: "#121213" }}>It's Awesome</span></p>
       <div>
         {aLL_nOTES.map((note) => <button onClick={() => handleChange(note)} disabled={currentGuess.length >= targetSong.length}>{note}</button>)}
-        <button onClick={() => removeNote()} disabled={currentGuess.length == 0} id="deleteButton">Delete</button>
-        <button onClick={() => removeAllNotes()} disabled={currentGuess.length == 0} id = "deleteAllButton">Delete All</button>
-        <button onClick={() => handleSubmit()} > Submit </button>
+        <button onClick={() => removeNote()} disabled={currentGuess.length == 0} className="sixCharsButton">Delete</button>
+        <button onClick={() => removeAllNotes()} disabled={currentGuess.length == 0} id="deleteAllButton">Delete All</button>
+        <button onClick={() => handleSubmit()} className="sixCharsButton">Submit</button>
       </div>
     </div>
   );
